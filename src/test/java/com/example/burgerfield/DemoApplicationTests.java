a07dbd70db11a6472aa769afc68511ab3a07b5d4
KristoferKSR <kaosaarkristofer@gmail.com>
2020-08-17 20:06:22 +0300

    Started development of Burgerfield

---
+++ src/test/java/com/example/burgerfield/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.burgerfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
